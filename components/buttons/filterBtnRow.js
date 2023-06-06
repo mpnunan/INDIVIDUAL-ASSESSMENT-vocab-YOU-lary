8a705edad6252aef9cd1cc9fb6337146a3ca2e1b
Michael Nunan <michael.nunan@outlook.com>
2023-06-05 19:59:31 -0500

    added navbar with minimal funcitonality

--- components/buttons/filterBtnRow.js
+++ components/buttons/filterBtnRow.js
@@ -3,36 +3,25 @@ import renderToDOM from '../../utils/renderToDom';
 
 const filterFunc = (uid) => {
   const btnValueArr = [];
-  const filterBtnRow = () => {
-    const stackArr = [];
-    getEntries(uid)
-      .then((entryArr) => entryArr.forEach((entryObj) => {
-        stackArr.push(entryObj.langOrTech);
-        stackArr.forEach((element) => {
-          if (btnValueArr.indexOf(element) === -1) {
-            btnValueArr.push(element);
-          }
-        });
-      }));
-    console.warn(btnValueArr);
-    return btnValueArr;
-  };
-
-  const filterBtnValues = () => {
-    console.warn(btnValueArr);
-    let filterBtnRowString = '';
-    for (let i = 0; i < btnValueArr.length; i++) {
-      filterBtnRowString += `
-      <div class="btn-group" role="group" aria-label="Basic radio toggle button group">
-      <input type="radio" class="btn-check" name="btnradio" id="btnradio${i}" autocomplete="off" checked>
-      <label class="btn btn-outline-primary" for="btnradio1">${btnValueArr[i]}</label>
-      </div>`;
-    }
-    renderToDOM('#filter-btns', filterBtnRowString);
-    console.warn(filterBtnRowString);
-  };
-  filterBtnRow();
-  filterBtnValues();
+  const stackArr = [];
+  getEntries(uid)
+    .then((entryArr) => entryArr.forEach((entryObj) => {
+      stackArr.push(entryObj.langOrTech);
+      stackArr.forEach((element) => {
+        if (btnValueArr.indexOf(element) === -1) {
+          btnValueArr.push(element);
+        }
+      });
+    }));
+  let filterBtnRowString = '';
+  for (let i = 0; i < btnValueArr.length; i++) {
+    filterBtnRowString += `
+    <div class="btn-group" role="group" aria-label="Basic radio toggle button group">
+      <input type="radio" class="btn-check" name="btnradio" id="btnradio--${btnValueArr[i]}" autocomplete="off" checked>
+      <label class="btn btn-outline-primary" for="btnradio--${btnValueArr[i]}">${btnValueArr[i]}</label>
+    </div>`;
+  }
+  renderToDOM('#filter-btns', filterBtnRowString);
 };
 
 // this function does not render anything
--- utils/startApp.js
+++ utils/startApp.js
@@ -1,10 +1,14 @@
 import getEntries from '../api/entryData';
 import logoutButton from '../components/logoutButton';
 import domBuilder from '../components/shared/domBuilder';
+import navbarOnDom from '../components/shared/navBar';
 import { showEntries } from '../pages/entries';
+import entryForm from '../pages/entryForm';
 
 const startApp = (user) => {
   domBuilder(user);
+  navbarOnDom(user);
+  entryForm();
   logoutButton();
   getEntries(user.uid).then((arr) => showEntries(arr, user));
 };
