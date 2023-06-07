8da9a5829e961d8761f3a0a4f6b88bff399f57fc
Michael Nunan <michael.nunan@outlook.com>
2023-06-07 15:58:56 -0500

    styling done

--- pages/entries.js
+++ pages/entries.js
@@ -12,17 +12,20 @@ const showEntries = (array) => {
   let domString = '';
   array.forEach((entry) => {
     domString += `
-    <div class="card" style="width: 18rem;">
-  <div class="card-body">
-    <h5 class="card-title">${entry.title}</h5>
-    <p class="card-text">${entry.description}.</p>
-    <a href="#" class="btn btn-primary" id="filter-entry--${entry.langOrTech}">${entry.langOrTech}</a>
-  </div>
-  <div id="btn-container-${entry.firebaseKey}">
-    <a href="#" class="btn btn-secondary" id ="update--${entry.firebaseKey}">Update Entry</a>
-    <a href="#" class="btn btn-secondary" id ="delete--${entry.firebaseKey}">Delete Entry</a>
-  </div>
-</div>`;
+    <div class="card">
+      <div class"card-title-container">
+        <h5 class="card-title">${entry.title}</h5>
+        <hr>
+      </div>
+      <div class="card-body">
+        <p class="card-text">${entry.description}.</p>
+        <a href="#" class="card-nav filter-link" id="filter-entry--${entry.langOrTech}">${entry.langOrTech}</a>
+      </div>
+      <div class="card-btn-container" id="btn-container-${entry.firebaseKey}">
+        <a href="#" class="card-nav update-link" id ="update--${entry.firebaseKey}">Update Entry</a>
+        <a href="#" class=" card-nav delete-link" id ="delete--${entry.firebaseKey}">Delete Entry</a>
+      </div>
+    </div>`;
   });
   renderToDOM('#card-container', domString);
 };
