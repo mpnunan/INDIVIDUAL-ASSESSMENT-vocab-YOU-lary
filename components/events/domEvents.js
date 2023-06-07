ac5ef5dbc2af37ab6b98de827f1b892cd7075a1d
Michael Nunan <michael.nunan@outlook.com>
2023-06-06 20:27:10 -0500

    delete button works

--- components/events/domEvents.js
+++ components/events/domEvents.js
@@ -1,11 +1,24 @@
-import { getSingleEntry } from '../../api/entryData';
+import { deleteEntry, getEntries, getSingleEntry } from '../../api/entryData';
+import { showEntries } from '../../pages/entries';
 import updateForm from '../../pages/updateForm';
 
-const domEvents = () => {
+const domEvents = (user) => {
   document.querySelector('#card-container').addEventListener('click', (e) => {
     if (e.target.id.includes('update--')) {
       const [, firebaseKey] = e.target.id.split('--');
       getSingleEntry(firebaseKey).then((entry) => updateForm(entry));
+    }
+
+    if (e.target.id.includes('delete--')) {
+      // eslint-disable-next-line no-alert
+      if (window.confirm('Not interested in learning this crucial piece of coding knowledge?')) {
+        console.warn('DESTROYING ENTRY', e.target.id);
+        const [, firebaseKey] = e.target.id.split('--');
+
+        deleteEntry(firebaseKey).then(() => {
+          getEntries(user.uid).then(showEntries);
+        });
+      }
     }
   });
 };
