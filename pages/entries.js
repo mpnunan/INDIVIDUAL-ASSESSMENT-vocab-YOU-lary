56e38d887ace625fd2aa48d0e619a1b900b45714
Michael Nunan <michael.nunan@outlook.com>
2023-06-06 20:10:58 -0500

    update functionality added

--- pages/entries.js
+++ pages/entries.js
@@ -18,6 +18,9 @@ const showEntries = (array) => {
     <p class="card-text">${entry.description}.</p>
     <a href="#" class="btn btn-primary">${entry.langOrTech}</a>
   </div>
+  <div id="update-form-container-${entry.firebaseKey}">
+    <a href="#" class="btn btn-secondary" id ="update--${entry.firebaseKey}">Update Entry</a>
+  </div>
 </div>`;
   });
   renderToDOM('#card-container', domString);
