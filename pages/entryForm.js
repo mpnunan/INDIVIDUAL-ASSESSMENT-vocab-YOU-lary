5ec2ae983d63e34d83fa751236c762d73d5fcac4
Michael Nunan <michael.nunan@outlook.com>
2023-06-11 19:47:03 -0500

    form stillallowing blank submissions

--- pages/entryForm.js
+++ pages/entryForm.js
@@ -2,10 +2,10 @@ import renderToDOM from '../utils/renderToDom';
 
 const entryForm = () => {
   const formString = `
-        <form id="entry-form">
-          <input type="text" class="create-update-entries form-control" id="input-name" placeholder="Word(s)" value="" required />
-          <input type="text" class="create-update-entries form-control" id="input-definition" placeholder="Definition" required />
-          <input type="text" class="create-update-entries form-control" id="input-langOrTech" placeholder="Language or Technology" required /> 
+        <form class="create-update-entry-form" id="entry-form">
+          <input type="text" class="create-update-entries form-control" name="input-name" id="input-name" placeholder="Word(s)" required />
+          <input type="text" class="create-update-entries form-control" name="input-definition" id="input-definition" placeholder="Definition" required />
+          <input type="text" class="create-update-entries form-control" name="input-langOrTech" id="input-langOrTech" placeholder="Language or Technology" required /> 
           <button type="submit" class="create-update-btn" id="submit-entry">Expand Vocabulary</button>
         </form>`;
   renderToDOM('#card-container', formString);
--- pages/updateForm.js
+++ pages/updateForm.js
@@ -2,10 +2,10 @@ import renderToDOM from '../utils/renderToDom';
 
 const updateForm = (obj) => {
   const updateString = `
-        <form id="update-form-${obj.firebaseKey}">
-          <input type="text" class="create-update-entries form-control" id="update-name" value="${obj.title}">
-          <input type="text" class="create-update-entries form-control" id="update-definition" value="${obj.description}">
-          <input type="text" class="create-update-entries form-control" id="update-langOrTech" value="${obj.langOrTech}">
+        <form class="create-update-entry-form" id="update-form-${obj.firebaseKey}">
+          <input type="text" class="create-update-entries form-control" name="update-name" id="update-name" value="${obj.title}">
+          <input type="text" class="create-update-entries form-control" name="update-definition" id="update-definition" value="${obj.description}">
+          <input type="text" class="create-update-entries form-control" name="update-langOrTech" id="update-langOrTech" value="${obj.langOrTech}">
           <button type="submit" class="create-update-btn" id="update-entry--${obj.firebaseKey}">Update Vocabulary</button>
         </form>`;
   renderToDOM('#card-container', updateString);
