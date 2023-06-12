8ce3abfae020c8053a44aa0997e4175723492382
Michael Nunan <michael.nunan@outlook.com>
2023-06-12 15:00:26 -0500

    still allowing blank form submissions

--- pages/entryForm.js
+++ pages/entryForm.js
@@ -3,9 +3,9 @@ import renderToDOM from '../utils/renderToDom';
 const entryForm = () => {
   const formString = `
         <form class="create-update-entry-form" id="entry-form">
-          <input type="text" class="create-update-entries form-control" name="input-name" id="input-name" placeholder="Word(s)" required />
-          <input type="text" class="create-update-entries form-control" name="input-definition" id="input-definition" placeholder="Definition" required />
-          <input type="text" class="create-update-entries form-control" name="input-langOrTech" id="input-langOrTech" placeholder="Language or Technology" required /> 
+          <input type="text" class="create-update-entries form-control" name="input-name" id="input-name" placeholder="Word(s)" minlength="1" required />
+          <input type="text" class="create-update-entries form-control" name="input-definition" id="input-definition" placeholder="Definition" minlength="1" required />
+          <input type="text" class="create-update-entries form-control" name="input-langOrTech" id="input-langOrTech" placeholder="Language or Technology" minlength="1" required /> 
           <button type="submit" class="create-update-btn" id="submit-entry">Expand Vocabulary</button>
         </form>`;
   renderToDOM('#card-container', formString);
